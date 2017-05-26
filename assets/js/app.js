afa8b8c06696b11416a7b472106f4198bf55dc49
Luz Gutierrez <mili01gm@gmail.com>
2017-05-26 17:45:26 -0500

    cambios varios para tratar de bloquear el pin inicial

--- assets/js/app.js
+++ assets/js/app.js
@@ -10,7 +10,34 @@ function initMap() {
     map: map
   });
 
-	var tarifa = document.getElementById("tarifa");
+  var miUbicacion;
+
+  function find(){
+    if(navigator.geolocation){
+      navigator.geolocation.getCurrentPosition(funcionExito, funcionError);
+    }
+
+    document.getElementById("encuentrame").addEventListener("click",find);
+
+    var latitud,longitud;
+    var funcionExito = function(position){
+      latitud = position.coords.latitude;
+      longitud = position.coords.longitude;
+    }
+    var miUbicacion = new google.maps.Marker({
+      position: {lat:latitud, lng:longitud},
+      map: map
+    });
+
+    map.setZoom(18);
+    map.setCenter({lat:latitud,lng:longitud});
+  }
+
+  var funcionError = function(error){
+    alert("Houston, tenemos un problema");
+  }
+
+  var tarifa = document.getElementById("tarifa");
   var partida = document.getElementById("partida");
   var llegada = document.getElementById("llegada");
   new google.maps.places.Autocomplete(partida);
@@ -28,12 +55,13 @@ function initMap() {
       if(status === 'OK'){
         var distancia = Number((response.routes[0].legs[0].distance.text.replace("km","")).replace(",","."));
         tarifa.classList.remove("none");
-				var costo = distancia*1.75;
+        var costo = distancia*1.75;
         if(costo>4){
           tarifa.innerHTML = "S/. " + parseInt(costo);
+          // miUbicacion.setMap("null");
         } else {
-					tarifa.innerHTML = "S/. 4";
-				}
+          tarifa.innerHTML = "S/. 4";
+        }
         console.log(response.routes[0].legs[0].distance.text);
         directionsDisplay.setDirections(response);
       } else {
@@ -47,30 +75,4 @@ function initMap() {
     calculateAndDisplayRoute(directionsService,directionsDisplay);
   };
   document.getElementById("trazar-ruta").addEventListener("click",trazarRuta);
-
-
-  function find(){
-    if(navigator.geolocation){
-      navigator.geolocation.getCurrentPosition(funcionExito, funcionError);
-    }
-
-    document.getElementById("encuentrame").addEventListener("click",find);
-
-    var latitud,longitud,miUbicacion;
-    var funcionExito = function(position){
-      latitud = position.coords.latitude;
-      longitud = position.coords.longitude;
-    }
-    var miUbicacion = new google.maps.Marker({
-      position: {lat:latitud, lng:longitud},
-      map: map
-    });
-
-    map.setZoom(18);
-    map.setCenter({lat:latitud,lng:longitud});
-  }
-
-  var funcionError = function(error){
-    alert("Houston, tenemos un problema");
-  }
 }
